5212310a935fa24a1c17ae9b5d2a7f472396124f
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-26 19:31:25 +0300

    fix: push stashed commits

---
+++ libs/elements/cards/src/lib/components/team-member-card/team-member-card.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-team-member-card',
+  templateUrl: './team-member-card.component.html',
+  styleUrls: ['./team-member-card.component.scss'],
+})
+export class TeamMemberCardComponent {}
